563ed226f389818f56990caeba7fbd4c1b23b472
TheBiggestThereIs <107923914+EthanDMello@users.noreply.github.com>
2022-07-26 17:06:36 +0100

    added function clearQuestion and added right or wrong to function dispalyQuestion

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,28 +1,80 @@
-const questionArea = document.querySelector(".question");
+const questionArea = document.querySelector(".questionForm");
 
 const question1 = {
-  question: "This is the first question?",
-  answer1: "This is the first answer",
-  answer2: "This is the second answer",
-  answer3: "This is the third answer",
+  question:
+    "Which built-in method returns the calling string value converted to upper case?",
+  answer1: "toUpperCase()",
+  answer2: "toUpper()",
+  answer3: "None of the above",
+  userAnswer: "",
+  correctAnswer: "answer1",
 };
 
-function displayBlock() {
+const question2 = {
+  question: "Question 2",
+  answer1: "toUpperCase()",
+  answer2: "toUpper()",
+  answer3: "None of the above",
+};
+
+const question3 = {
+  question: "Question 3",
+  answer1: "toUpperCase()",
+  answer2: "toUpper()",
+  answer3: "None of the above",
+};
+
+const question4 = {
+  question: "question 4",
+  answer1: "toUpperCase()",
+  answer2: "toUpper()",
+  answer3: "None of the above",
+};
+
+const questionAr = [question1, question2, question3, question4];
+
+function displayBlock(questions) {
   // take object to display and add to HTML
   let el = document.createElement("section");
   //   add question
-  el.textContent = question1.question;
+  el.textContent = questions.question;
   questionArea.appendChild(el);
   //   find answers
-  const keys = Object.keys(question1);
-  console.log(keys);
+  const keys = Object.keys(questions);
   keys.forEach((key) => {
-    if (key === "question") return;
-    console.log("not question", key);
-    answerEl = document.createElement("section");
-    answerEl.textContent = question1[key];
-    questionArea.appendChild(answerEl);
+    if (key.startsWith("answer")) {
+      // add answers to HTML
+      answerEl = document.createElement("button");
+      answerEl.textContent = questions[key];
+      answerEl.setAttribute("type", "submit");
+      answerEl.setAttribute("answerId", key);
+      questionArea.appendChild(answerEl);
+    }
   });
 }
 
-displayBlock();
+function clearQuestion() {
+  questionArea.innerHTML = " ";
+}
+
+function displayQuestion(question) {
+  displayBlock(question);
+  let buttons = document.querySelectorAll("button");
+  console.log(buttons);
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      answerId = button.getAttribute("answerId");
+      question["userAnswer"] = answerId;
+      console.log(answerId + " submit");
+      if (question["userAnswer"] === question["correctAnswer"]) {
+        console.log("right answer");
+      } else {
+        console.log("wrong answer");
+      }
+      clearQuestion();
+    });
+  });
+}
+
+displayQuestion(question1);
